2cde152529737521c976e7a484114cfbf5094c50
Tamanna@5092 <tamannaaktermoni51@gmailcom>
2024-11-04 12:50:11 +0600

    slide and tabs

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import Carousel from '../component/Carousel';
+import TabCategories from '../component/TabCategories';
 
 const Home = () => {
     return (
         <div>
-            
+            <Carousel></Carousel>
+            <TabCategories></TabCategories>
         </div>
     );
 };
