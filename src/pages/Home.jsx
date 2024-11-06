634f4b7c5ee082446c8c889c28f10dc253e4dcba
Tamanna@5092 <tamannaaktermoni51@gmailcom>
2024-11-06 23:04:06 +0600

    jobcard tabs

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import Carousel from '../component/Carousel';
 import TabCategories from '../component/TabCategories';
-import { useLoaderData } from 'react-router-dom';
 
 const Home = () => {
-    const jobs = useLoaderData()
-    console.log(jobs)
     return (
         <div>
             <Carousel></Carousel>
-            <TabCategories jobs={jobs}></TabCategories>
+            <TabCategories></TabCategories>
         </div>
     );
 };
